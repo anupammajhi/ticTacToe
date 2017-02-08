e7ead7767b6457b80f110aea30fafd74e196a2b4
Anupam Majhi <majhi.anupam@gmail.com>
2017-02-08 16:09:16 +0530

    Update

--- script.js
+++ script.js
@@ -2,30 +2,62 @@ $(function(){
 
 })
 
+// Create array of size 9 with blanks
+arr = ["0","1","2","3","4","5","6","7","8"]
+winArr = []
+choice = "X"
+
+console.log("Test Line");
+arr[3] = "X" //ONLY FOR TESTING
+arr[6] = "AnupamW"
+
 function checkWins(letter){
-  if( $(".row1 .col1").text() == letter && $(".row1 .col2").text() == letter && $(".row1 .col3").text() == letter ){
+  if( arr[0] == letter && arr[1] == letter && arr[2] == letter ){
     return true;
   }
-  if( $(".row2 .col1").text() == letter && $(".row2 .col2").text() == letter && $(".row2 .col3").text() == letter ){
+  if( arr[3] == letter && arr[4] == letter && arr[5] == letter ){
     return true;
   }
-  if( $(".row3 .col1").text() == letter && $(".row3 .col2").text() == letter && $(".row3 .col3").text() == letter ){
+  if( arr[6] == letter && arr[7] == letter && arr[8] == letter ){
     return true;
   }
-  if( $(".row1 .col1").text() == letter && $(".row2 .col1").text() == letter && $(".row3 .col1").text() == letter ){
+  if( arr[0] == letter && arr[3] == letter && arr[6] == letter ){
     return true;
   }
-  if( $(".row1 .col2").text() == letter && $(".row2 .col2").text() == letter && $(".row3 .col2").text() == letter ){
+  if( arr[1] == letter && arr[4] == letter && arr[7] == letter ){
     return true;
   }
-  if( $(".row1 .col3").text() == letter && $(".row2 .col3").text() == letter && $(".row3 .col3").text() == letter ){
+  if( arr[2] == letter && arr[5] == letter && arr[8] == letter ){
     return true;
   }
-  if( $(".row1 .col1").text() == letter && $(".row2 .col2").text() == letter && $(".row3 .col3").text() == letter ){
+  if( arr[0] == letter && arr[4] == letter && arr[8] == letter ){
     return true;
   }
-  if( $(".row3 .col1").text() == letter && $(".row2 .col2").text() == letter && $(".row1 .col3").text() == letter ){
+  if( arr[2] == letter && arr[4] == letter && arr[6] == letter ){
     return true;
   }
   return false;
+}
+
+function buildWinArray(){
+  winArr = arr.slice();
+  holeArr = winArr.filter(function(ele){
+    // That doesn't have a X or O or a prediction ending with W
+     return !(ele.slice(-1) == "X" || ele.slice(-1) == "O" || ele.slice(-1) == "W")
+  });
+  holeArr.forEach(function(possiblePath){
+    possiblePath.split("").forEach(function(){
+
+    });
+  });
+}
+
+// AI here
+
+function ticTacAI(){
+
+
+  // Filter Empty Boxes into an array Arr
+  // check worst
+
 }
