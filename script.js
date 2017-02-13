68f1ec0e3fc47b4625ffa7a00560e7e42730f15d
Anupam Majhi <majhi.anupam@gmail.com>
2017-02-14 02:28:26 +0530

    Better

--- script.js
+++ script.js
@@ -22,9 +22,14 @@ function displayChance() {
         if (gameplay == "1 Player" && chance == 0) {
             text = "Computer's Chance"
             if (game[0].length == 0) {
+                console.log("CALCULATING POSSIBLE MOVES =============================");
                 calculatePossibleMoves();
             }
-            computerPlay();
+            console.log("Sleeping");
+            setTimeout(function() {
+                computerPlay();
+            }, 1000)
+
         }
         if (gameplay == "1 Player" && chance == 1) {
             text = "Your Chance"
@@ -91,7 +96,6 @@ $(".col1, .col2, .col3").click(function() {
         game[1].push(clickedBoxIndex)
         chance == 0 ? chance = 1 : chance = 0;
         display();
-        console.log(game)
     }
 });
 
@@ -99,6 +103,7 @@ possibleMovesAI = []
 possibleMovesPlayer = []
 possibleMovesWins = []
 possibleMovesDraw = []
+possibleMovesLost = []
 MovesLeft = true;
 statusArr = []
 
@@ -132,182 +137,220 @@ function checkWin(checkArr) {
 function calculatePossibleMoves() {
     // If human player plays first
     if (arr.indexOf(playerChoice) >= 0 && possibleMovesPlayer.length == 0) {
+        console.log("Human Played First");
         possibleMovesPlayer.push([
             [],
             [arr.indexOf(playerChoice)], "LIVE"
         ])
     }
-
+    console.log("Game Array :")
     console.log(game)
     //Start prediction of AI Move
-    if (possibleMovesPlayer.length > 0 && MovesLeft) {
-        possibleMovesAI = []
-        console.log("AI Moves");
-        possibleMovesPlayer.forEach(function(moveArray) {
-            emptyBoxes = [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(function(ele) {
-                if (moveArray[0].concat(moveArray[1]).indexOf(ele) == -1) {
-                    return true;
+    console.log(possibleMovesPlayer.length);
+    console.log(MovesLeft);
+    if (MovesLeft) {
+        if (possibleMovesPlayer.length > 0) {
+            possibleMovesAI = []
+            console.log("Predicting AI Moves");
+            possibleMovesPlayer.forEach(function(moveArray) {
+                emptyBoxes = [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(function(ele) {
+                    if (moveArray[0].concat(moveArray[1]).indexOf(ele) == -1) {
+                        return true;
+                    }
+                });
+
+                // console.log(emptyBoxes);
+                if (emptyBoxes.length == 1) {
+                    MovesLeft = false;
                 }
+
+                emptyBoxes.forEach(function(ele) {
+
+                    if (checkWin(moveArray[0].concat([ele]))) {
+                        status = "WON"
+                    } else if (((moveArray[0].concat([ele])).concat(moveArray[1])).length == 9) {
+                        status = "DRAW"
+                        // console.log(((moveArray[0].concat([ele])).concat(moveArray[1])));
+                    } else {
+                        status = "LIVE"
+                    }
+
+                    if (status == "WON") {
+                        possibleMovesWins.push([moveArray[0].concat([ele]), moveArray[1], status])
+                    } else if (status == "LIVE") {
+                        possibleMovesAI.push([moveArray[0].concat([ele]), moveArray[1], status])
+                        // console.log(moveArray[0].concat([ele]), moveArray[1], status);
+                    } else if (status == "DRAW") {
+                        possibleMovesDraw.push([moveArray[0].concat([ele]), moveArray[1], status])
+                    }
+
+                    statusArr.push(status)
+
+                })
+            });
+        } else {
+            randomVal = Math.floor((Math.random() * 8));
+            console.log("Computer plays first - Hence Random Value");
+            console.log([randomVal], [], "LIVE");
+            possibleMovesAI.push([
+                [randomVal],
+                [], "LIVE"
+            ])
+
+            game[0].push(randomVal);
+            display2();
+        }
+
+
+        // Start Prediction of Player Move
+        //Start prediction
+        if (possibleMovesAI.length > 0) {
+            possibleMovesPlayer = []
+            console.log("Predicting Player Moves");
+            possibleMovesAI.forEach(function(moveArray) {
+                emptyBoxes = [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(function(ele) {
+                    if (moveArray[0].concat(moveArray[1]).indexOf(ele) == -1) {
+                        return true;
+                    }
+                });
+
+                if (emptyBoxes.length == 1) {
+                    MovesLeft = false;
+                }
+
+                emptyBoxes.forEach(function(ele) {
+
+                    if (checkWin(moveArray[1].concat([ele]))) {
+                        status = "LOST"
+                    } else if (((moveArray[0].concat([ele])).concat(moveArray[1])).length == 9) {
+                        status = "DRAW"
+                    } else {
+                        status = "LIVE"
+                    }
+
+                    if (status == "LIVE") {
+                        // console.log(moveArray[0], moveArray[1].concat([ele]), status)
+                        possibleMovesPlayer.push([moveArray[0], moveArray[1].concat([ele]), status])
+                    } else if (status == "DRAW") {
+                        possibleMovesDraw.push([moveArray[0], moveArray[1].concat([ele]), status])
+                    } else if (status == "LOST") {
+                        possibleMovesLost.push([moveArray[0], moveArray[1].concat([ele]), status])
+                    }
+                    statusArr.push(status)
+                })
             });
 
-            // console.log(emptyBoxes);
-            if (emptyBoxes.length == 1) {
-                MovesLeft = false;
-            }
+            calculatePossibleMoves()
 
-            emptyBoxes.forEach(function(ele) {
-
-                if (checkWin(moveArray[0].concat([ele]))) {
-                    status = "WON"
-                } else if (((moveArray[0].concat([ele])).concat(moveArray[1])).length == 9) {
-                    status = "DRAW"
-                    // console.log(((moveArray[0].concat([ele])).concat(moveArray[1])));
-                } else {
-                    status = "LIVE"
-                }
-
-                if (status == "WON") {
-                    possibleMovesWins.push([moveArray[0].concat([ele]), moveArray[1], status])
-                } else if (status == "LIVE") {
-                    possibleMovesAI.push([moveArray[0].concat([ele]), moveArray[1], status])
-                    // console.log(moveArray[0].concat([ele]), moveArray[1], status);
-                } else if (status == "DRAW") {
-                    possibleMovesDraw.push([moveArray[0].concat([ele]), moveArray[1], status])
-                }
-
-                statusArr.push(status)
-
-            })
-        });
+        }
     } else {
-        randomVal = Math.floor((Math.random() * 8));
-        console.log("AI Moves");
-        console.log([randomVal], [], "LIVE");
-        possibleMovesAI.push([
-            [randomVal],
-            [], "LIVE"
-        ])
-
-        game[0].push(randomVal);
-        display2();
-    }
-
-
-    // Start Prediction of Player Move
-    //Start prediction
-    if (possibleMovesAI.length > 0 && MovesLeft) {
-        possibleMovesPlayer = []
-        console.log("Player Moves");
-        possibleMovesAI.forEach(function(moveArray) {
-            emptyBoxes = [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(function(ele) {
-                if (moveArray[0].concat(moveArray[1]).indexOf(ele) == -1) {
-                    return true;
-                }
-            });
-
-            if (emptyBoxes.length == 1) {
-                MovesLeft = false;
-            }
-
-            emptyBoxes.forEach(function(ele) {
-
-                if (checkWin(moveArray[1].concat([ele]))) {
-                    status = "LOST"
-                } else if (((moveArray[0].concat([ele])).concat(moveArray[1])).length == 9) {
-                    status = "DRAW"
-                } else {
-                    status = "LIVE"
-                }
-
-                if (status == "LIVE") {
-                    // console.log(moveArray[0], moveArray[1].concat([ele]), status)
-                    possibleMovesPlayer.push([moveArray[0], moveArray[1].concat([ele]), status])
-                } else if (status == "DRAW") {
-                    possibleMovesDraw.push([moveArray[0], moveArray[1].concat([ele]), status])
-                }
-                statusArr.push(status)
-            })
-        });
-
-        calculatePossibleMoves()
-
-    } else {
-        console.log("Completed Prediction");
+        console.log("COMPLETED PREDICTING ========================");
     }
 
 }
 
 
 function computerPlay() {
-    console.log("Computer Playing")
-    console.log("Start game :"+ game[0] + " -- " game[1]);
-    setTimeout(function(){console.log("Sleeping")},1000)
-    if(game[0].length == 1 && game[1].length == 0)
-    {
-              arr.splice(game[0][0], 1, choice)
-              chance == 0 ? chance = 1 : chance = 0;
-              display()
-    }
-    else if ((game[0].length == 0 && game[1].length == 1) || game[0].length >= 1) {
+    console.log("Computer Playing /////////////////////////////////////")
+    console.log("Start game :" + game[0] + " -- " + game[1]);
+    if (game[0].length == 1 && game[1].length == 0) {
+        arr.splice(game[0][0], 1, choice)
+        chance == 0 ? chance = 1 : chance = 0;
+        display()
+    } else if ((game[0].length == 0 && game[1].length == 1) || game[0].length >= 1) {
 
         playIndex = game[0].length
 
-        filterWin = possibleMovesWins.filter(function(arrEle) {
-            return game[0].every(function(ele, index) {
-                return arrEle[0][index] == ele;
-            }) && game[1].every(function(ele, index) {
-                return arrEle[1][index] == ele;
-            })
+        console.log("Testing Bad Moves ----");
+
+        filterLost = possibleMovesLost.filter(function(ele) {
+            return game[0].every(function(num, index) {
+                return num == ele[0][index]
+            }) && game[1].every(function(num, index) {
+                return num == ele[1][index]
+            }) && ele[0].length == game[0].length + 1
         })
 
-        if (filterWin.length > 0) {
-            //Found data in Win Matrix, continue further
+        if (filterLost.length > 0) {
+
+            console.log("BAD MOVE FOUND");
+
+            filterLostList = filterLost.map(function(ele) {
+                return ele[0][playIndex]
+            })
+            choosePlace = [0, 1, 2, 3, 4, 5, 6, 7, 8].filter(function(num) {
+                return filterLostList.indexOf(num) < 0 && arr.indexOf(num.toString()) >= 0
+            });
+
+            choosePlace = choosePlace[0]
         } else {
-            //No data in Win Matrix, hence try Draw Matrix
-            filterWin = possibleMovesDraw.filter(function(arrEle) {
+            console.log("No Bad Move Found ---");
+            filterWin = possibleMovesWins.filter(function(arrEle) {
                 return game[0].every(function(ele, index) {
                     return arrEle[0][index] == ele;
                 }) && game[1].every(function(ele, index) {
                     return arrEle[1][index] == ele;
                 })
             })
+
+            if (filterWin.length > 0) {
+                //Found data in Win Matrix, continue further
+            } else {
+                //No data in Win Matrix, hence try Draw Matrix
+                filterWin = possibleMovesDraw.filter(function(arrEle) {
+                    return game[0].every(function(ele, index) {
+                        return arrEle[0][index] == ele;
+                    }) && game[1].every(function(ele, index) {
+                        return arrEle[1][index] == ele;
+                    })
+                })
+            }
+
+            filterPlay = filterWin.sort(function(a, b) {
+                //Sort by minimum number of moves by computer
+                return a[0].length - b[0].length
+            })
+
+            filterPlayBestMoves = filterPlay.filter(function(ele) {
+                //filter by minimum number of moves only
+                return ele[0].length == filterPlay[0][0].length
+            })
+
+            filterPlayBestMovesStrategic = filterPlayBestMoves.filter(function(ele) {
+                //Filter even digit places as they are best strategic moves with double threatening etc
+                return ele[0][playIndex] % 2 == 0
+            })
+
+            if (filterPlayBestMovesStrategic.length != 0) {
+                filterPlayBestMoves = filterPlayBestMovesStrategic;
+            }
+
+            //Choose one of the move matrix randomly
+
+            chooseMatrixIndex = Math.round((Math.random()) * (filterPlayBestMoves.length - 1))
+
+            console.log("Best Moves Available :")
+            console.log(filterPlayBestMoves);
+
+            choosePlace = filterPlayBestMoves[chooseMatrixIndex][0][playIndex]
+            console.log("Choose Best Place: " + choosePlace);
         }
 
-        filterPlay = filterWin.sort(function(a, b) {
-            //Sort by minimum number of moves by computer
-            return a[0].length - b[0].length
-        })
 
-        filterPlayBestMoves = filterPlay.filter(function(ele) {
-            //filter by minimum number of moves only
-            return ele[0].length == filterPlay[0][0].length
-        })
 
-        filterPlayBestMovesStrategic = filterPlayBestMoves.filter(function(ele) {
-            //Filter even digit places as they are best strategic moves with double threatening etc
-            return ele[0][playIndex] % 2 == 0
-        })
-
-        if (filterPlayBestMovesStrategic.length != 0) {
-            filterPlayBestMoves = filterPlayBestMovesStrategic;
-        }
-
-        //Choose one of the move matrix randomly
-
-        chooseMatrixIndex = Math.round((Math.random()) * (filterPlayBestMoves.length - 1))
-
-        console.log(filterPlayBestMoves)
-
-        choosePlace = filterPlayBestMoves[chooseMatrixIndex][0][playIndex]
-        console.log(choosePlace)
 
         arr.splice(choosePlace, 1, choice)
         game[0].push(choosePlace)
         chance == 0 ? chance = 1 : chance = 0;
 
         display();
+
+
+
+
     }
-    console.log("End game :"+ game[0] + " -- " game[1]);
+    console.log("End game :" + game[0] + " -- " + game[1]);
+    console.log("Computer Played ////////////////////////////////////");
 }
 
 
