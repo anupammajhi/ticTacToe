2016f28de39e35dc75bdf64acb04dc866fc15786
Anupam Majhi <majhi.anupam@gmail.com>
2017-02-14 16:44:31 +0530

    Rule 1,2,5 done

--- script.js
+++ script.js
@@ -256,10 +256,10 @@ function computerPlay() {
 
 
       // [x] Rule 1: If I have a winning move, take it.
-      // [ ] Rule 2: If the opponent has a winning move, block it.
+      // [X] Rule 2: If the opponent has a winning move, block it.
       // [ ] Rule 3: If I can create a fork (two winning ways) after this move, do it.
       // [ ] Rule 4: Do not let the opponent creating a fork after my move. (Opponent may block your winning move and create a fork.)
-      // [ ] Rule 5: Place in the position such as I may win in the most number of possible ways.
+      // [X] Rule 5: Place in the position such as I may win in the most number of possible ways.
 
 
     console.log("Computer Playing /////////////////////////////////////")
